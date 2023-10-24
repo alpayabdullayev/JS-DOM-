7e5f410392373bad5fb3ea6c40b65198287bed2c
alpayabdullayev <alpayabdullayev0@gmail.com>
2023-10-24 13:27:30 +0400

    DOM

---
+++ assets/js/scripts.js
@@ -0,0 +1,73 @@
+// let paraqraf = document.createElement('div');
+// paraqraf.innerHTML = "salam";
+
+// let info = document.querySelector('.info');
+// info.appendChild(paraqraf);
+
+
+
+
+// setTimeout(( )=>{
+//     let body = document.querySelector('body')
+//     let div = document.createElement('div')
+//     div.style.width = "400px"
+//     div.style.height = "400px"
+//     body.append(div)
+//     div.style.backgroundColor = "yellow"
+// },1000)
+
+
+
+
+// let main = document.getElementById('main')
+
+// let prev = main.previousElementSibling
+// let next = main.nextElementSibling
+
+// console.log(prev, next);
+
+// let box = document.getElementById('box')
+// let childNode = box.lastElementChild
+
+// console.log(childNode);
+
+
+
+// document.getElementById("box").onclick = function() {
+//     var box = document.getElementById("box");
+  
+// };
+
+
+// let div = document.createElement('div')
+// let btn = document.createElement('button')
+// let body = document.querySelector('body')
+// btn.textContent = 'click'
+
+
+// div.style.width= '200px'
+// div.style.height= '200px'
+// div.style.backgroundColor = 'red'
+
+// body.append(div)
+// body.append(btn)
+
+// btn.onclick=()=>{
+//     div.classList.toggle('animation')
+//     div.style.backgroundColor = 'yellow'
+// } m
+
+
+
+
+// let div = document.getElementById('box')
+// div.classList.add('active')
+// div.classList.add('test1' , 'col')
+
+// div.classList.remove('active')
+
+
+// let find =div.classList.contains('col')
+// console.log(find);
+
+
